264122c04aab6089ec1f0c12af834d0f6fae1c52
Jose manuel Lopez pena <jolopez-@c2r9s1.42madrid.com>
2023-09-23 20:15:56 +0200

    tokenizer ok, probar

--- src/lexer/tokenizer.c
+++ src/lexer/tokenizer.c
@@ -6,78 +6,120 @@
 /*   By: jolopez- <jolopez-@student.42madrid>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/19 17:51:52 by jolopez-          #+#    #+#             */
-/*   Updated: 2023/09/23 13:58:25 by jolopez-         ###   ########.fr       */
+/*   Updated: 2023/09/23 19:23:06 by jolopez-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-int	ft_is_space(char *line, int *i)
+static void	ft_get_tokens(char *line, t_token *tokens, int *i, int *j)
 {
-	return (line[*i] == ' ' || line[*i] == '\t' || line[*i] == '\v');
+	int	flag;
+
+	flag = 1;
+	while (line[*i])
+	{
+		if (ft_is_space(line, i))
+		{
+			*i = *i + 1;
+			flag = 1;
+		}
+		else if (ft_strchr("()\'\"*;<>|&", line[*i]))
+		{
+			ft_token(tokens, line, i, j);
+			flag = 1;
+		}
+		else if (flag == 1)
+		{
+			tokens[*j] = tk_word;
+			*j = *j + 1;
+			*i = *i + 1;
+			flag = 0;
+		}
+		else if (flag == 0)
+			*i = *i + 1;
+	}
+}
+
+static void	ft_check_sgltoken(char *line, int *i, int *flag, int *tokens)
+{
+	if (ft_strchr("()\'\"*;<>|&", line[*i]))
+	{
+		*i = *i + 1;
+		*tokens = *tokens + 1;
+		*flag = 1;
+	}
+	else if (*flag == 1)
+	{
+		*tokens = *tokens + 1;
+		*flag = 0;
+	}
+	else if (*flag == 0)
+		*i = *i + 1;
+}
+
+static void	ft_check_dbltoken(char *line, int *i, int *flag, int *tokens)
+{
+	char	*str;
+
+	str = ft_substr(line, *i, 2);
+	if (ft_strncmp("<<", str, 2) == 0 || ft_strncmp(">>", str, 2) == 0)
+	{
+		*i = *i + 2;
+		*tokens = *tokens + 1;
+		*flag = *flag + 1;
+	}
+	else if (ft_strncmp("||", str, 2) == 0)
+	{
+		*i = *i + 2;
+		*tokens = *tokens + 1;
+		*flag = *flag + 1;
+	}
+	else if (ft_strncmp("&!", str, 2) == 0 || ft_strncmp("&&", str, 2) == 0)
+	{
+		*i = *i + 2;
+		*tokens = *tokens + 1;
+		*flag = *flag + 1;
+	}
+	else
+		ft_check_sgltoken(line, i, flag, tokens);
+	free(str);
 }
 
 static int	ft_count_tokens(char *line)
 {
-	int	i;
-	int	flag;
-	int	tokens;
+	int		i;
+	int		flag;
+	int		tokens;
 
 	i = 0;
 	flag = 1;
 	tokens = 0;
 	while (line[i])
 	{
-		if (ft_is_space(line, i))
+		if (ft_is_space(line, &i))
 		{
 			i++;
 			flag = 1;
 		}
-		else if (ft_strchr("()\'\"*;<>|&", line[i]))
-		{
-			i++;
-			tokens++;
-			flag = 1;
-		}
-		else if (flag == 1)
-		{
-			tokens++;
-			flag = 0;
-		}
-		else if (flag == 0)
-			i++;
+		else
+			ft_check_dbltoken(line, &i, &flag, &tokens);
 	}
 	return (tokens);
 }
 
-t_token	*ft_tokenizer(char *line)
+t_token	*ft_tokenizer(char *line, int *tk_number)
 {
 	int		i;
 	int		j;
-	int		flag;
 	t_token	*tokens;
 
 	i = 0;
 	j = 0;
-	flag = 0;
-	tokens = malloc(sizeof(int) * ft_count_tokens(line));
-	while (line[i])
-	{
-		if (ft_is_space(line, i))
-			i++;
-		else if (!!ft_strchr("()\'\"*;<>|&", line[i]))
-		{
-			ft_token(tokens, line, &i, &j);
-			flag = 1;
-		}
-		else if (flag == 0)
-		{
-			tokens[j] = tk_word;
-			j++;
-			i++;
-		}
-		else if (flag == 1)
-			i++;
-	}
+	*tk_number = ft_count_tokens(line);
+	tokens = malloc(sizeof(int) * *tk_number);
+	if (!tokens)
+		exit(1);
+	ft_get_tokens(line, tokens, &i, &j);
 	return (tokens);
 }
--- src/utils/tools.c
+++ src/utils/tools.c
@@ -1,35 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   tokenizer_2 copy.c                                 :+:      :+:    :+:   */
+/*   tools.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: jolopez- <jolopez-@student.42madrid>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/09/21 20:36:04 by jolopez-          #+#    #+#             */
-/*   Updated: 2023/09/22 20:41:08 by jolopez-         ###   ########.fr       */
+/*   Created: 2023/09/23 16:32:16 by jolopez-          #+#    #+#             */
+/*   Updated: 2023/09/23 16:32:30 by jolopez-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-int	add_separator_token(t_csh *tcsh, t_oken **tokens, char *line, size_t *i)
+int	ft_is_space(char *line, int *i)
 {
-	size_t				j;
-	t_oken				*new;
-
-	j = 0;
-	(void)tcsh;
-	while (g_separators[j] != NULL)
-	{
-		if (ft_strnequ(g_separators[j], &line[*i], ft_strlen(g_separators[j])))
-		{
-			new = create_token(tokens);
-			if (new == NULL)
-				return (1);
-			*i += ft_strlen(g_separators[j]);
-			new->str = ft_strdup(g_separators[j]);
-		}
-		j++;
-	}
-	return (0);
+	return (line[*i] == ' ' || line[*i] == '\t' || line[*i] == '\v');
 }

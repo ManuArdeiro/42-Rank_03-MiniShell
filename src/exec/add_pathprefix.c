2710d76b3fefcd0e86f4d976d10126bb3b386010
git@vogsphere-v2.42madrid.com:vogsphere/intra-uuid-32ab929b-05de-4d78-bd4a-2e205f0fd26d-3987085-yzaytoun <yzaytoun@c2r2s3.42madrid.com>
2023-10-17 20:48:51 +0200

    WIP

--- src/exec/add_pathprefix.c
+++ src/exec/add_pathprefix.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   add_pathprefix.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yzaytoun <yzaytoun@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/17 20:37:20 by yzaytoun          #+#    #+#             */
+/*   Updated: 2023/10/17 20:47:04 by yzaytoun         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "minishell.h"
 
 char	*ft_add_pathprefix(char *commandname, t_list *envlist)
@@ -6,6 +18,7 @@ char	*ft_add_pathprefix(char *commandname, t_list *envlist)
 	char	*path;
 	int		count;
 	char	*buffer;
+	char	*buffer2;
 
 	count = 0;
 	buffer = NULL;
@@ -13,7 +26,9 @@ char	*ft_add_pathprefix(char *commandname, t_list *envlist)
 	pathlist = ft_split(path, ':');
 	while (pathlist[count] != NULL)
 	{
-		buffer = ft_strjoin(pathlist[count], commandname);
+		buffer2 = ft_strjoin("/", commandname);
+		buffer = ft_strjoin(pathlist[count], buffer2);
+		free(buffer2);
 		if (access(buffer, X_OK) == 0)
 		{
 			free(commandname);

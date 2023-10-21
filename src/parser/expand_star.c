edea5b15100193f71de1356de8bc004934ac4d91
git@vogsphere-v2.42madrid.com:vogsphere/intra-uuid-32ab929b-05de-4d78-bd4a-2e205f0fd26d-3987085-yzaytoun <yzaytoun@c2r2s3.42madrid.com>
2023-10-21 12:34:32 +0200

    Finised Star expansion

--- src/parser/expand_star.c
+++ src/parser/expand_star.c
@@ -6,11 +6,30 @@
 /*   By: yzaytoun <yzaytoun@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/20 18:05:17 by yzaytoun          #+#    #+#             */
-/*   Updated: 2023/10/20 18:58:17 by yzaytoun         ###   ########.fr       */
+/*   Updated: 2023/10/21 12:32:55 by yzaytoun         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
+
+static int	ft_countchar(const char *string, int c)
+{
+	int	count;
+	int	i;
+
+	count = 0;
+	i = 0;
+	if (string)
+	{
+		while (string[i] != 0)
+		{
+			if (string[i] == (char)c)
+				count++;
+			i++;
+		}
+	}
+	return (count);
+}
 
 t_list	*ft_expand_startoken(char *fullpath)
 {
@@ -18,20 +37,25 @@ t_list	*ft_expand_startoken(char *fullpath)
 	DIR				*directory;
 	struct dirent	*dirent;
 	t_list			*files_list;
+	int				lastpos;
 
 	files_list = NULL;
 	if (path == NULL)
 		return (NULL);
-	path = ft_strrchr(fullpath, "/");
+	lastpos = ft_countchar(fullpath, '/');
+	path = ft_strchr_pos(fullpath, '/', lastpos);
 	if (path == NULL)
 		path = ".";
 	printf("path = %s\n", path);
 	directory = opendir(path);
-	dirent = readdir(directory);
+	if (directory != NULL)
+		dirent = readdir(directory);
 	while (dirent != NULL)
 	{
-		if (dirent != NULL)
-			ft_lstinsert(&files_list, (char *)dirent->d_name, BACK);
+		ft_lstinsert(
+			&files_list,
+			ft_strjoin(path, (char *)dirent->d_name), BACK);
 		dirent = readdir(directory);
 	}
+	return (files_list);
 }

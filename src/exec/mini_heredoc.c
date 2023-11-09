f4b63482ef196f79719909e7659fd8611685366e
git@vogsphere-v2.42madrid.com:vogsphere/intra-uuid-32ab929b-05de-4d78-bd4a-2e205f0fd26d-3987085-yzaytoun <yzaytoun@c2r2s3.42madrid.com>
2023-11-09 19:31:35 +0100

    WIP

--- src/exec/mini_heredoc.c
+++ src/exec/mini_heredoc.c
@@ -6,7 +6,7 @@
 /*   By: yzaytoun <yzaytoun@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/04 18:28:55 by yzaytoun          #+#    #+#             */
-/*   Updated: 2023/11/08 18:47:13 by yzaytoun         ###   ########.fr       */
+/*   Updated: 2023/11/09 19:10:36 by yzaytoun         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -45,13 +45,25 @@ void	ft_writetofile(const char *delimiter, int *herepipe)
 	}
 }
 
+static void	ft_wait_and_close(int child, t_file *file, int *herepipe)
+{
+	int		status;
+
+	status = EXIT_SUCCESS;
+	if (dup2(herepipe[0], STDIN_FILENO) < 0)
+		ft_printerror(__func__, "DUP2");
+	ft_close_pipes(&file->fd[0], &file->fd[1]);
+	ft_close_pipes(&herepipe[0], &herepipe[1]);
+	if (waitpid(child, &status, EXIT_SUCCESS) < 0)
+		ft_printerror(__func__, "Wait");
+	ft_evaluate_subprocess(status);
+}
+
 void	ft_get_heredoc(t_file **file)
 {
 	pid_t	child;
-	int		status;
 	int		herepipe[2];
 
-	status = 0;
 	if (pipe(herepipe) < 0)
 		ft_printerror(__func__, "Pipe");
 	if (pipe((*file)->fd) < 0)
@@ -61,18 +73,9 @@ void	ft_get_heredoc(t_file **file)
 	{
 		ft_close_pipes(&(*file)->fd[0], &(*file)->fd[1]);
 		ft_writetofile((*file)->name, herepipe);
-		printf("Inside execute\n");
 	}
 	else if (child < 0)
 		ft_printerror(__func__, "Fork");
 	else
-	{
-		if (dup2(herepipe[0], (*file)->fd[1]) < 0)
-			ft_printerror(__func__, "DUP2");
-		ft_close_pipes(&herepipe[0], &herepipe[1]);
-		ft_close_pipes(&(*file)->fd[0], &(*file)->fd[1]);
-	}
-	if (waitpid(child, &status, EXIT_SUCCESS) < 0)
-		ft_printerror(__func__, "Wait");
-	ft_evaluate_subprocess(status);
+		ft_wait_and_close(child, *file, herepipe);
 }

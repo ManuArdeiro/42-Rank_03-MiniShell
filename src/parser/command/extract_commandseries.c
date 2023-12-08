34414f1faef13f1acbc64395549631fd8b3f10f7
git@vogsphere-v2.42madrid.com:vogsphere/intra-uuid-32ab929b-05de-4d78-bd4a-2e205f0fd26d-3987085-yzaytoun <yzaytoun@c2r2s3.42madrid.com>
2023-12-08 20:07:31 +0100

    WIP

--- src/parser/command/extract_commandseries.c
+++ src/parser/command/extract_commandseries.c
@@ -6,26 +6,47 @@
 /*   By: yzaytoun <yzaytoun@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/16 20:29:13 by yzaytoun          #+#    #+#             */
-/*   Updated: 2023/12/08 19:42:42 by yzaytoun         ###   ########.fr       */
+/*   Updated: 2023/12/08 20:07:16 by yzaytoun         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
+static t_part	*ft_get_last_seriestoken(t_part *tokenlist)
+{
+	t_part	*node;
+
+	if (tokenlist == NULL)
+		return (NULL);
+	node = tokenlist;
+	while (node->next != NULL)
+	{
+		if (node->next->token == tk_space)
+			return (node);
+		node = node->next;
+	}
+	return (node);
+}
+
 static char	*ft_get_series_substring(const char *commandline, t_part **node)
 {
 	t_part	*secondnode;
 	char	*commandseries;
+	char	*buffer;
 
 	commandseries = NULL;
 	secondnode = ft_get_tokennode(
 			(*node)->next, ft_get_tokenpair((*node)->token), FALSE, FIRST);
+	if (secondnode != NULL
+		&& secondnode->next != NULL && secondnode->next->token != tk_space)
+		secondnode = ft_get_last_seriestoken(secondnode);
 	if (secondnode != NULL && secondnode->end > (*node)->start)
-		commandseries
-			= ft_substr(
-				commandline,
-				(*node)->start + 1,
-				(secondnode->end - 1) - ((*node)->start + 1));
+	{
+		buffer = ft_substr(commandline,
+					(*node)->start + 1,
+					((secondnode->end) - ((*node)->start) + 1));
+		commandseries = ft_strclean_withspaces(buffer, TRUE);
+	}
 	(*node) = secondnode;
 	return (commandseries);
 }

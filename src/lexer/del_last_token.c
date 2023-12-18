6d5b3d6ae582a8e54388cecafd5e1597642aea7d
ManuArdeiro <ardeiro@gmail.com>
2023-12-18 20:46:39 +0100

    giuo

--- src/lexer/del_last_token.c
+++ src/lexer/del_last_token.c
@@ -6,7 +6,7 @@
 /*   By: jolopez- <jolopez-@student.42madrid>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/18 20:20:09 by jolopez-          #+#    #+#             */
-/*   Updated: 2023/12/18 20:44:32 by jolopez-         ###   ########.fr       */
+/*   Updated: 2023/12/18 20:46:06 by jolopez-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,10 +18,4 @@ void	ft_del_last_tkn(t_part *tokens)
 
 	(void)antelast_tk;
 	(void)tokens;
-
-	/*antelast_tk = tokens;
-	while (last_tk->next->next != NULL)
-	{
-		if (last_tk->next->)
-		last_tk = last_tk->next;*/
 }
